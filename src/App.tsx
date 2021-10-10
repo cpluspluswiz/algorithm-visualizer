9e3f6602162c8ba73b8b9da30e7b9702c2c3ea80
cpluspluswiz <muhammadabdullahlm10hd@gmail.com>
2021-10-10 06:12:07 -0400

    bubbleSort complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './App.css';
 
 function App() {
   const [elements, setElements] = useState([20, 23, 45, 65, 23, 32, 50, 200, 23.4, 54, 23, 111, 42, 18]);
-  const [sliderValue, setSliderValue] = useState(14);
+  const [arraySize, setArraySize] = useState(14);
+  const [sortingSpeed, setSortingSpeed] = useState(200);
+  const [started, setStarted] = useState(false);
+  const [stop, setStop] = useState(false);
 
   const selectedAlgorithm = "Buble Sort";
 
@@ -16,38 +19,93 @@ function App() {
     }
     setElements(newArray);
   };
-  const sizeSliderOnChange = () => {
+  const arraySizeSliderOnChange = () => {
     let arraySize = parseInt((document?.getElementById("size-slider") as HTMLInputElement)?.value);
-    setSliderValue(arraySize);
+    setArraySize(arraySize);
     arrayGenerator(arraySize);
   }
 
-  const generateNewArrayOnClick = () => {
-    arrayGenerator(sliderValue)
+  const arraySpeedSliderOnChange = () => {
+    let arraySpeed = parseInt((document?.getElementById("speed-slider") as HTMLInputElement)?.value);
+    setSortingSpeed(arraySpeed);
   }
 
-  const algorithmOnClick = () => {
+  const generateNewArrayOnClick = () => {
+    arrayGenerator(arraySize);
+  }
+  const delay = (ms:number) => new Promise(res => setTimeout(res, ms));
+  const swapArrayEl = (arr:number[], idx:number) => {
+    let temp = arr[idx + 1];
+    arr[idx + 1] = arr[idx];
+    arr[idx] = temp;
+  }
 
+  /* ALGORITHMS */
+
+  const bubbleSort = async (speed:number) => {
+    setStarted(true);
+    let isSorted = false;
+    let newArray = elements;
+    while(!isSorted){
+      if(stop){
+        console.log(stop);
+        setStarted(false);
+        return;
+      };
+      console.log(stop);
+      isSorted = true;
+      for(let i = 0; i < newArray.length - 1; ++i) {
+        const arr1 = document.querySelectorAll<HTMLElement>('.array-element')[i];
+        const arr2 = document.querySelectorAll<HTMLElement>('.array-element')[i + 1];
+        arr1.style.background = "green";
+        arr2.style.background = "green";
+        await delay(200);
+        if(newArray[i] > newArray[i + 1]){
+            swapArrayEl(newArray, i);
+            arr1.style.background = "black";
+            arr2.style.background = "grey";
+            await delay(200);
+            arr1.classList.add("slide-right")
+            arr2.classList.add("slide-left")
+            await delay(200);
+            setElements([...newArray]);
+            arr1.style.background = "grey";
+            arr2.style.background = "black";
+            arr1.classList.remove("slide-right")
+            arr2.classList.remove("slide-left")
+            arr1.style.background = "#A61B1B";
+            arr2.style.background = "#A61B1B";
+            isSorted = false;
+        }else{
+          arr1.style.background = "#A61B1B";
+          arr2.style.background = "#A61B1B";
+        }
+      }
+    }
+    setStarted(false);
   }
 
   return (
     <div className="app">
       <div className="header">
-        <input id="generate-array-btn" className="btn" type="button" value="Generate a New Array" onClick={generateNewArrayOnClick}/>
+        <input id="generate-array-btn" className={`btn ${started ? "btn-disabled" : ""}`} disabled={started} type="button" value="Generate a New Array" onClick={generateNewArrayOnClick}/>
+        {/* <button className="btn" onClick={() => {
+          setStop(true);
+        }}>Stop</button> */}
         <div className="algorithms-container">
           <p>Choose an algorithm</p>
           <div className="algorithms-list">
-            <input className="btn algorithm-btn" type="button" value="Buble sort" onClick={algorithmOnClick}/>
-            <input className="btn algorithm-btn" type="button" value="Quick Sort" onClick={algorithmOnClick}/>
-            <input className="btn algorithm-btn" type="button" value="Merge Sort" onClick={algorithmOnClick}/>
-            <input className="btn algorithm-btn" type="button" value="Heap Sort" onClick={algorithmOnClick}/>
+            <input className={`btn algorithm-btn ${started ? "btn-disabled" : ""}`} disabled={started} type="button" value="Buble sort" onClick={() => bubbleSort(sortingSpeed)}/>
+            <input className={`btn algorithm-btn ${started ? "btn-disabled" : ""}`} disabled={started} type="button" value="Quick Sort" />
+            <input className={`btn algorithm-btn ${started ? "btn-disabled" : ""}`} disabled={started} type="button" value="Merge Sort" />
+            <input className={`btn algorithm-btn ${started ? "btn-disabled" : ""}`} disabled={started} type="button" value="Heap Sort" />
           </div>
         </div>
         <div className="algorithms-params">
           <p>Set Speed</p>
-          <input className="slider" type="range" min="1" max="150" value="20" />
+          <input id="speed-slider" className={`slider ${started ? "slider-disabled" : ""}`} disabled={started} type="range" min="10" max="400" value={sortingSpeed} onChange={arraySpeedSliderOnChange}/>
           <p>Set Size</p>
-          <input id="size-slider" className="slider" type="range" min="1" max="150" value={sliderValue} onChange={sizeSliderOnChange}/>
+          <input id="size-slider" className={`slider ${started ? "slider-disabled" : ""}`} disabled={started} type="range" min="1" max="150" value={arraySize} onChange={arraySizeSliderOnChange}/>
         </div>
       </div>
       <div className="elements-wrapper">
